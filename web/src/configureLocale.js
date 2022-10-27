13e458f16880925bcbf31a1bb8b5c37568d1bded
Gribou <toutain@numericable.com>
2022-10-27 11:15:03 +0200

    Delete web directory

--- web/src/configureLocale.js
+++
@@ -1,9 +0,0 @@
-import * as moment from "moment-timezone";
-import "moment/locale/fr";
-
-const configureLocale = () => {
-  moment.locale("fr");
-  moment.tz.setDefault("UTC");
-};
-
-export default configureLocale;
